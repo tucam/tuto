94f606e5a2a707cbd3887d330ba0fd8b4e347f0d
Tuan Anh DANG <Tuan Anh DANG>
2016-09-01 22:07:42 +0200

    init commit

---
+++ MapTreeView/MapTreeView/ItemViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapTreeView
+{
+    /// <summary>
+    /// Model of tree item.
+    /// </summary>
+    class ItemViewModel : INotifyPropertyChanged
+    {
+        private ItemViewModel parent;
+        private string name;
+        private ObservableCollection<ItemViewModel> children;
+
+        public ItemViewModel(ItemViewModel parent, string name)
+        {
+            this.parent = parent;
+            this.name = name;
+            children = new ObservableCollection<ItemViewModel>();
+        }
+        /// <summary>
+        /// Item's parent
+        /// </summary>
+        public ItemViewModel Parent
+        {
+            get { return parent; }
+            set
+            {
+                if (!Equals(parent, value))
+                {
+                    parent = value;
+                    OnPropertyChanged("Parent");
+                }
+            }
+        }
+        /// <summary>
+        /// Name of item
+        /// </summary>
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+        /// <summary>
+        /// Item's children
+        /// </summary>
+        public ObservableCollection<ItemViewModel> Children { get { return children; } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MapTreeView/MapTreeView/TreeViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapTreeView
+{
+    class TreeViewModel
+    {
+        private ObservableCollection<ItemViewModel> items;
+
+        public TreeViewModel()
+        {
+            items = new ObservableCollection<ItemViewModel>();
+            BuildItemViewModels();
+        }
+
+        public ObservableCollection<ItemViewModel> Items
+        {
+            get { return items; }
+        }
+        private void BuildItemViewModels()
+        {
+            // level 1
+            for (int i = 0; i < 3; i++)
+            {
+                ItemViewModel itemLevel1 = new ItemViewModel(null, string.Format("Item {0}", i + 1));
+                // level 2
+                for (int j = 0; j < 3; j++)
+                {
+                    ItemViewModel itemLevel2 = new ItemViewModel(itemLevel1, string.Format("Item {0}.{1}", i + 1, j + 1));
+                    // level 3
+                    for (int k = 0; k < 2; k++)
+                    {
+                        ItemViewModel itemLevel3 = new ItemViewModel(itemLevel2, string.Format("Item {0}.{1}.{2}", i + 1, j + 1, k + 1));
+
+                        itemLevel2.Children.Add(itemLevel3);
+                        items.Add(itemLevel3);
+                    }
+
+                    itemLevel1.Children.Add(itemLevel2);
+                    items.Add(itemLevel2);
+                }
+
+                items.Add(itemLevel1);
+            }
+        }
+    }
+}
