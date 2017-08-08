9219c1dadd5764ec9b36712fd5a15f8c78f3ddbc
Tuan Anh DANG <Tuan Anh DANG>
2017-08-08 22:31:45 +0200

    add empty map

--- Java.E4Geotools/src/e4geotools/parts/SamplePart.java
+++ Java.E4Geotools/src/e4geotools/parts/SamplePart.java
@@ -1,63 +1,33 @@
 package e4geotools.parts;
 
-import java.util.Arrays;
-import java.util.List;
 import javax.annotation.PostConstruct;
-import javax.inject.Inject;
 
 import org.eclipse.e4.ui.di.Focus;
-import org.eclipse.e4.ui.di.Persist;
-import org.eclipse.e4.ui.model.application.ui.MDirtyable;
-import org.eclipse.jface.viewers.ArrayContentProvider;
-import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.ModifyEvent;
-import org.eclipse.swt.events.ModifyListener;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Text;
+import org.geotools.map.MapContent;
+import org.geotools.swt.SwtMapPane;
 
 public class SamplePart {
-
-	private Text txtInput;
-	private TableViewer tableViewer;
-
-	@Inject
-	private MDirtyable dirty;
+	private SwtMapPane mapPane;
 
 	@PostConstruct
 	public void createComposite(Composite parent) {
-		parent.setLayout(new GridLayout(1, false));
+		// create map content
+		MapContent mapContent = new MapContent();
 
-		txtInput = new Text(parent, SWT.BORDER);
-		txtInput.setMessage("Enter text to mark part as dirty");
-		txtInput.addModifyListener(new ModifyListener() {
-			@Override
-			public void modifyText(ModifyEvent e) {
-				dirty.setDirty(true);
-			}
-		});
-		txtInput.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		// create map view
+		mapPane = new SwtMapPane(parent, SWT.BORDER | SWT.NO_BACKGROUND);
+		mapPane.setMapContent(mapContent);
 
-		tableViewer = new TableViewer(parent);
-
-		tableViewer.setContentProvider(ArrayContentProvider.getInstance());;
-		tableViewer.setInput(createInitialDataModel());
-		tableViewer.getTable().setLayoutData(new GridData(GridData.FILL_BOTH));
+		// set the renderer
+		// StreamingRenderer renderer = new StreamingRenderer();
+		// mapPane.setRenderer(renderer);
 	}
 
 	@Focus
 	public void setFocus() {
-		tableViewer.getTable().setFocus();
+		mapPane.setFocus();
 	}
 
-	@Persist
-	public void save() {
-		dirty.setDirty(false);
-	}
-	
-	private List<String> createInitialDataModel() {
-		return Arrays.asList("Sample item 1", "Sample item 2", "Sample item 3", "Sample item 4", "Sample item 5");
-	}
 }
